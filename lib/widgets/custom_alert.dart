a328dfe1aa98c6da1817f6d563739c61b845c1d1
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-12-14 13:07:31 +0300

    improve messaging on popup alert

--- lib/widgets/custom_alert.dart
+++ lib/widgets/custom_alert.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CustomPopUpWidget extends ConsumerWidget {
-  const CustomPopUpWidget({super.key, required this.popUpText});
+  const CustomPopUpWidget(
+      {super.key, required this.popUpTitle, required this.popUpText});
+  final String popUpTitle;
   final String popUpText;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Builder(builder: (context) {
       return AlertDialog(
+        title: Text(popUpTitle),
         content: SingleChildScrollView(
           child: Text(popUpText),
         ),
