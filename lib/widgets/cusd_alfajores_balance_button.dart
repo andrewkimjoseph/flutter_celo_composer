b8147caf9cd047109313686da4cda96dd392ded6
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-12-14 16:10:07 +0300

    widget: improve cusd_alfajores_balance_button

--- lib/widgets/cusd_alfajores_balance_button.dart
+++ lib/widgets/cusd_alfajores_balance_button.dart
@@ -3,56 +3,93 @@ import 'package:flutter_celo_composer/widgets/progress_indicator.dart';
 import 'package:reown_appkit/appkit_modal.dart';
 
 import '../services/celo_composer_services.dart';
-import 'custom_alert.dart';
 
-class CUSDAlfajoresBalanceButton extends StatefulWidget {
-  const CUSDAlfajoresBalanceButton({super.key, required this.appKit});
+class CUSDBalanceButton extends StatefulWidget {
+  const CUSDBalanceButton({
+    super.key,
+    required this.appKit,
+    this.forTestnet = true,
+  });
 
   final ReownAppKitModal appKit;
+  final bool forTestnet;
 
   @override
-  State<CUSDAlfajoresBalanceButton> createState() =>
-      _CUSDAlfajoresBalanceButtonState();
+  State<CUSDBalanceButton> createState() => _CUSDBalanceButtonState();
 }
 
-class _CUSDAlfajoresBalanceButtonState
-    extends State<CUSDAlfajoresBalanceButton> {
+class _CUSDBalanceButtonState extends State<CUSDBalanceButton> {
   bool isCheckingBalance = false;
+  late CeloComposerServices _celoComposerServices;
+
+  final String btnTextForAlfajores = "Check cUSD Balance on Alfajores";
+  final String btnTextForMainnet = "Check cUSD Balance on Mainnet";
+
+  @override
+  void initState() {
+    super.initState();
+    _celoComposerServices = CeloComposerServices(widget.appKit);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 4),
       child: ElevatedButton(
-          onPressed: _checkcUSDBalanceOnAlfajores,
-          child: isCheckingBalance
-              ? const CustomProgressIndicator()
-              : const Text("Check cUSD balance on Alfajores")),
+        onPressed: isCheckingBalance ? null : _checkcUSDBalance,
+        child: isCheckingBalance
+            ? const CustomProgressIndicator()
+            : Text(widget.forTestnet ? btnTextForAlfajores : btnTextForMainnet),
+      ),
     );
   }
 
-  void _checkcUSDBalanceOnAlfajores() async {
+  Future<void> _checkcUSDBalance() async {
     setState(() {
       isCheckingBalance = true;
     });
-    double balance =
-        await CeloComposerServices.checkcUSDBalanceAlfajores(widget.appKit);
 
-    debugPrint("cUSD Balance on Alfajores: $balance cUSD");
+    try {
+      double balance = widget.forTestnet
+          ? await _celoComposerServices.checkcUSDBalanceOnAlfajores()
+          : await _celoComposerServices.checkcUSDBalanceOnMainnet();
 
-    if (mounted) {
-      setState(() {
-        isCheckingBalance = false;
-      });
-
-      showDialog(
-          context: context,
-          builder: (context) {
-            return CustomPopUpWidget(
-              popUpTitle: 'cUSD Balance on Alfajores',
-              popUpText: '$balance cUSD',
-            );
-          });
+      debugPrint("cUSD Balance: $balance cUSD");
+      _showBalanceSnackbar(balance);
+    } catch (e) {
+      _showErrorSnackbar(e.toString());
+    } finally {
+      if (mounted) {
+        setState(() {
+          isCheckingBalance = false;
+        });
+      }
     }
+  }
+
+  void _showBalanceSnackbar(double balance) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'cUSD Balance: $balance cUSD',
+          style: const TextStyle(color: Colors.black),
+        ),
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.yellow,
+      ),
+    );
+  }
+
+  void _showErrorSnackbar(String errorMessage) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Error: $errorMessage',
+          style: const TextStyle(color: Colors.yellow),
+        ),
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.black,
+      ),
+    );
   }
 }
